46482bc83de6031e21d5328a08d9dccc85a81018
pnichols789 <pnichols789@gmail.com>
2022-11-15 16:26:16 -0600

    -

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,6 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom"
 
 const PrivateRouter=({isAuth}) => {
+
+    if (!isAuth){
+        alert('You must be logged in to view that page')
+    }
+
     return( isAuth ? <Outlet/> : <Navigate to='/login'/>)
 }
 
